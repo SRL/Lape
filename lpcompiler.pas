2c9ec8e2e3a6ab8b9e9e14bd7084c5abd3b4c291
John Peel <john@dgby.org>
2013-09-23 16:21:02 -0400

    Lape: Allow read access to TLapeCompiler.FBaseDefines.

--- lpcompiler.pas
+++ lpcompiler.pas
@@ -242,6 +242,7 @@ type
     property Importing: Boolean read getImporting write setImporting;
   published
     property Tokenizer: TLapeTokenizerBase read getTokenizer write setTokenizer;
+    property BaseDefines: TStringList read FBaseDefines;
     property Defines: TStringList read FDefines write setBaseDefines;
     property OnHandleDirective: TLapeHandleDirective read FOnHandleDirective write FOnHandleDirective;
     property OnHandleExternal: TLapeHandleExternal read FOnHandleExternal write FOnHandleExternal;
